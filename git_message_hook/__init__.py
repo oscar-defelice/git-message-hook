4382e0d55abcc54e33a0acedb6c6c91cd84f865a
oscar-defelice <oscar.defelice@gmail.com>
2023-02-17 10:13:32 +0100

    chore: Update version

--- git_message_hook/__init__.py
+++ git_message_hook/__init__.py
@@ -3,6 +3,6 @@ __email__ = "oscar.defelice@gmail.com"
 __copyright__ = "(c) 2022 Oscar de Felice"
 
 __license__ = "MIT"
-__version__ = "1.0.3"
-__title__ = 'GIT Message Hook'
-__description__ = 'Force conventional git commit messages for git repositories, by leveraging git hooks.'
+__version__ = "1.0.5"
+__title__ = "GIT Message Hook"
+__description__ = "Force conventional git commit messages for git repositories, by leveraging git hooks."
